8c9e373012a6ec01a0cb0ba58cc664447f60c1ed
Evan Krambuhl <thekrambuhl@gmail.com>
2016-03-31 01:03:12 -0700

    error fix!

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const splitTrees = require('rogain-utils').splitTrees;
 
 module.exports = function Range(tree, props) {
-  var split = splitTrees(tree, { type: 'component', name: 'Else' });
+  var split = splitTrees(tree.children, { type: 'component', name: 'Else' });
   var data = parseInt(tree.data, 10);
   var min = parseInt(tree.attrs.min, 10);
   var max = parseInt(tree.attrs.max, 10);
